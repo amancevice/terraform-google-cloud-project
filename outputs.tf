1df41c415d164978097831fb8dfd2c0303c26c44
Alexander Mancevice <amancevice@cargometrics.com>
2018-06-20 08:22:07 -0400

    outputs

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "bucket_name" {
+  description = "Cloud Storage bucket name for Terraform state files."
+  value       = "${google_storage_bucket.bucket.name}"
+}
+
+output "project_id" {
+  description = "Google Cloud Project "
+  value       = "${google_project.project.project_id}"
+}
+
+output "services" {
+  description = "Google Cloud Project enabled API services."
+  value       = ["${google_project_services.project.services}"]
+}
